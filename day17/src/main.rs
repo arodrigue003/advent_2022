ca1e0da0f75ce03511c78cb35b71709b1c1cb40b
arodrigue003 <adrien.rod@aliceadsl.fr>
2023-11-11 17:03:32 +0100

    Add: Add day 17 part 02 solution

--- day17/src/main.rs
+++ day17/src/main.rs
@@ -1,4 +1,7 @@
 use lazy_static::lazy_static;
+use std::collections::hash_map::DefaultHasher;
+use std::collections::HashMap;
+use std::hash::{Hash, Hasher};
 use std::ops::{Index, IndexMut};
 use std::{env, fs};
 
@@ -7,6 +10,13 @@ static GRID_HEIGHT: usize = 50_000;
 struct Shape {
     shape: Vec<Point>,
     height: usize,
+}
+
+#[derive(Debug, Eq, PartialEq, Clone)]
+struct FrontLine {
+    start: usize,
+    low: usize,
+    end: usize,
 }
 
 lazy_static! {
@@ -178,7 +188,7 @@ impl FrontLineDirection {
     }
 }
 
-#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+#[derive(Debug, Eq, PartialEq, Copy, Clone, Hash)]
 enum GridState {
     Air,
     Rock,
@@ -237,21 +247,18 @@ impl Grid {
     fn shift_grid(&mut self) {
         let front_line = self.compute_front_line();
 
-        // Front line is the height of the start and end line in the grid.
-        // We need to convert them to grid offset in order to perform our operation
-        let front_line_start = front_line.0 - self.y_offset;
-        let front_line_end = front_line.1 - self.y_offset;
-
         // If we cannot remove elements from the grid, we are fucked
-        if front_line_start == 0 || front_line_end == 1 {
+        if front_line.start - self.y_offset == 0 || front_line.end - self.y_offset == 1 {
             // We can fix this by having the grid height being dynamic and just allocating
             // new lines without removing old ones.
             // We can also solve this issue by increasing the base height
             unimplemented!()
         }
 
+        // Front line is the height of the start and end line in the grid.
+        // We need to convert them to grid offset in order to perform our operation
         // We remove everything bellow the front_line
-        let to_remove = front_line_start.min(front_line_end);
+        let to_remove = front_line.start.min(front_line.end).min(front_line.low) - self.y_offset;
         self.grid.drain(0..to_remove);
 
         // We now add new lines above the front line
@@ -315,7 +322,7 @@ impl Grid {
         false
     }
 
-    fn compute_front_line(&self) -> (usize, usize) {
+    fn compute_front_line(&self) -> FrontLine {
         // Get the position of the first element of the front line
         let mut starting = Point {
             x: 0,
@@ -329,6 +336,9 @@ impl Grid {
             starting.y -= 1
         }
 
+        // Keep the information about the front line min height and the path
+        let mut path: Vec<Point> = vec![];
+
         // Try to reach the right wall
         let mut current = starting.clone();
         let mut direction = FrontLineDirection::Right;
@@ -338,6 +348,13 @@ impl Grid {
             // If we can go left, turn left and update the position and the direction
             if let Some(left) = left {
                 if self[(left.x, left.y)] == GridState::Rock {
+                    // If we are going back, remove the last entry from the path
+                    if path.len() >= 2 && path[path.len() - 2] == current {
+                        path.pop();
+                    } else {
+                        path.push(current);
+                    }
+
                     current = left;
                     direction = direction.turn_left();
                     continue;
@@ -347,6 +364,13 @@ impl Grid {
             // If we can go front, advance of one case
             if let Some(front) = front {
                 if self[(front.x, front.y)] == GridState::Rock {
+                    // If we are going back, remove the last entry from the path
+                    if path.len() >= 2 && path[path.len() - 2] == current {
+                        path.pop();
+                    } else {
+                        path.push(current);
+                    }
+
                     current = front;
                     continue;
                 }
@@ -356,7 +380,25 @@ impl Grid {
             direction = direction.turn_right();
         }
 
-        (starting.y, current.y)
+        FrontLine {
+            start: starting.y,
+            low: path.iter().map(|point| point.y).min().unwrap(),
+            end: current.y,
+        }
+    }
+
+    fn get_front_line_hash(&self) -> u64 {
+        let front_line = self.compute_front_line();
+
+        // Front line is the height of the start and end line in the grid.
+        // We need to convert them to grid offset in order to perform our operation
+        let min_height = front_line.start.min(front_line.end).min(front_line.low) - self.y_offset;
+        let max_height = self.max_height - self.y_offset;
+        let mut hasher = DefaultHasher::new();
+        for y in min_height..max_height {
+            self.grid[y].hash(&mut hasher);
+        }
+        hasher.finish()
     }
 }
 
@@ -414,8 +456,75 @@ fn solve_part_one(directions: &[Direction]) {
     println!("Part one solution: {}", grid.max_height - 1);
 }
 
-fn solve_part_two(data: &str) {
-    println!("Part two solution:");
+fn solve_part_two(directions: &[Direction]) {
+    let target = 1_000_000_000_000;
+
+    let mut grid = Grid::new();
+    let mut shape_iterator = SHAPES.iter().cycle();
+    let mut fallen_shapes = 0;
+
+    // Initialize shape and it's position
+    let mut current_shape = shape_iterator.next().unwrap();
+    let mut current_shape_pos = Point {
+        x: 2,
+        y: grid.max_height + 3,
+    };
+
+    let direction_len = directions.len();
+    let mut step = 0;
+    let shape_len = SHAPES.len();
+    let mut step_shape = 0;
+
+    let mut to_check: HashMap<(usize, usize, u64), (usize, usize)> = HashMap::new();
+    let mut test = true;
+
+    for direction in directions.iter().cycle() {
+        // Push the shape with the wind
+        grid.push(current_shape, &mut current_shape_pos, *direction);
+        let is_stopped = grid.fall(current_shape, &mut current_shape_pos);
+        if is_stopped {
+            // Increase the count of fallen shapes
+            fallen_shapes += 1;
+
+            // If we reached the target, break the loop
+            if fallen_shapes == target {
+                break;
+            }
+
+            // If we reach a synchronisation point, print it
+            if test {
+                let new_check = (step, step_shape, grid.get_front_line_hash());
+                if let Some((last_fallen_shapes, last_max_height)) = to_check.get(&new_check) {
+                    let shape_skip = fallen_shapes - last_fallen_shapes;
+                    let max_height_skip = grid.max_height - last_max_height;
+
+                    // Compute the number of skip we can perform
+                    let skip_count = (target - fallen_shapes) / shape_skip;
+
+                    // Do the skip
+                    fallen_shapes += skip_count * shape_skip;
+                    grid.max_height += skip_count * max_height_skip;
+                    grid.y_offset += skip_count * max_height_skip;
+                    test = false;
+                }
+                to_check.insert(new_check, (fallen_shapes, grid.max_height));
+            }
+
+            // the shape reached a stop position, spawn a new one
+            current_shape = shape_iterator.next().unwrap();
+            step_shape = (step_shape + 1) % shape_len;
+            current_shape_pos = Point {
+                x: 2,
+                y: grid.max_height + 3,
+            };
+        }
+
+        step = (step + 1) % direction_len;
+    }
+
+    // grid.pretty_print(false);
+
+    println!("Part two solution: {}", grid.max_height - 1);
 }
 
 fn main() {
@@ -439,5 +548,5 @@ fn main() {
         .collect();
 
     solve_part_one(&directions);
-    solve_part_two(&data);
+    solve_part_two(&directions);
 }
