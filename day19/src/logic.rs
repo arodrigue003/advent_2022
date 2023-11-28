0266aaf8043898defe2d8f709fed6c0a2d594938
arodrigue003 <adrien.rod@aliceadsl.fr>
2023-11-28 19:12:18 +0100

    Add: Add day 19 part 02 solution

--- day19/src/logic.rs
+++ day19/src/logic.rs
@@ -1,4 +1,5 @@
-use crate::models::{Blueprint, BuildOption, ForbiddenBuilds, Game, MaxTime, Resources, Robots};
+use crate::models::{Blueprint, BuildOption, Game, MaxTime};
+use rayon::prelude::*;
 
 impl Game {
     pub fn build_options(
@@ -50,7 +51,7 @@ impl Game {
         ]
     }
 
-    pub fn tick_with_build_option(&mut self, build_option: BuildOption, blueprint: &Blueprint) {
+    pub fn tick(&mut self, build_option: BuildOption, blueprint: &Blueprint) {
         // Update resources
         self.resources.ore += self.robots.ore;
         self.resources.clay += self.robots.clay;
@@ -88,318 +89,6 @@ impl Game {
             self.forbidden_builds.geode = false;
         }
     }
-
-    pub fn tick(&self, build_option: BuildOption, blueprint: &Blueprint) -> Option<Self> {
-        match build_option {
-            BuildOption::None => Some(Self {
-                robots: Robots {
-                    ore: self.robots.ore,
-                    clay: self.robots.clay,
-                    obsidian: self.robots.obsidian,
-                    geode: self.robots.geode,
-                },
-                resources: Resources {
-                    ore: self.resources.ore + self.robots.ore,
-                    clay: self.resources.clay + self.robots.clay,
-                    obsidian: self.resources.obsidian + self.robots.obsidian,
-                    geode: self.resources.geode + self.robots.geode,
-                },
-                forbidden_builds: ForbiddenBuilds {
-                    ore: self.forbidden_builds.ore,
-                    clay: self.forbidden_builds.clay,
-                    obsidian: self.forbidden_builds.obsidian,
-                    geode: self.forbidden_builds.geode,
-                },
-            }),
-            BuildOption::Ore => {
-                if !self.forbidden_builds.ore && self.resources.ore >= blueprint.ore {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore + 1,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.ore,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Clay => {
-                if !self.forbidden_builds.clay && self.resources.ore >= blueprint.clay {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay + 1,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.clay,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Obsidian => {
-                if !self.forbidden_builds.obsidian
-                    && self.resources.ore >= blueprint.obsidian.0
-                    && self.resources.clay >= blueprint.obsidian.1
-                {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian + 1,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.obsidian.0,
-                            clay: self.resources.clay + self.robots.clay - blueprint.obsidian.1,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Geode => {
-                if !self.forbidden_builds.geode
-                    && self.resources.ore >= blueprint.geode.0
-                    && self.resources.obsidian >= blueprint.geode.1
-                {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode + 1,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.geode.0,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian
-                                - blueprint.geode.1,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-        }
-    }
-
-    pub fn tick_heuristic(&self, build_option: BuildOption, blueprint: &Blueprint) -> Option<Self> {
-        match build_option {
-            BuildOption::None => Some(Self {
-                robots: Robots {
-                    ore: self.robots.ore,
-                    clay: self.robots.clay,
-                    obsidian: self.robots.obsidian,
-                    geode: self.robots.geode,
-                },
-                resources: Resources {
-                    ore: self.resources.ore + self.robots.ore,
-                    clay: self.resources.clay + self.robots.clay,
-                    obsidian: self.resources.obsidian + self.robots.obsidian,
-                    geode: self.resources.geode + self.robots.geode,
-                },
-                forbidden_builds: ForbiddenBuilds {
-                    ore: self.forbidden_builds.ore,
-                    clay: self.forbidden_builds.clay,
-                    obsidian: self.forbidden_builds.obsidian,
-                    geode: self.forbidden_builds.geode,
-                },
-            }),
-            BuildOption::Ore => {
-                if !self.forbidden_builds.ore && self.resources.ore >= blueprint.ore {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore + 1,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.ore,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Clay => {
-                if !self.forbidden_builds.clay && self.resources.ore >= blueprint.clay {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay + 1,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.clay,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: false,
-                            clay: false,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Obsidian => {
-                if !self.forbidden_builds.obsidian
-                    && self.resources.ore >= blueprint.obsidian.0
-                    && self.resources.clay >= blueprint.obsidian.1
-                {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian + 1,
-                            geode: self.robots.geode,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.obsidian.0,
-                            clay: self.resources.clay + self.robots.clay - blueprint.obsidian.1,
-                            obsidian: self.resources.obsidian + self.robots.obsidian,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: true,
-                            clay: true,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-            BuildOption::Geode => {
-                if !self.forbidden_builds.geode
-                    && self.resources.ore >= blueprint.geode.0
-                    && self.resources.obsidian >= blueprint.geode.1
-                {
-                    Some(Self {
-                        robots: Robots {
-                            ore: self.robots.ore,
-                            clay: self.robots.clay,
-                            obsidian: self.robots.obsidian,
-                            geode: self.robots.geode + 1,
-                        },
-                        resources: Resources {
-                            ore: self.resources.ore + self.robots.ore - blueprint.geode.0,
-                            clay: self.resources.clay + self.robots.clay,
-                            obsidian: self.resources.obsidian + self.robots.obsidian
-                                - blueprint.geode.1,
-                            geode: self.resources.geode + self.robots.geode,
-                        },
-                        forbidden_builds: ForbiddenBuilds {
-                            ore: true,
-                            clay: true,
-                            obsidian: false,
-                            geode: false,
-                        },
-                    })
-                } else {
-                    None
-                }
-            }
-        }
-    }
-}
-
-fn simulate_game_rec(
-    game: &Game,
-    current_time: usize,
-    time: usize,
-    blueprint: &Blueprint,
-) -> usize {
-    if current_time == time {
-        return game.resources.geode * blueprint.index;
-    }
-
-    // Get the game for every build option
-    let ore = game.tick(BuildOption::Ore, blueprint);
-    let clay = game.tick(BuildOption::Clay, blueprint);
-    let obsidian = game.tick(BuildOption::Obsidian, blueprint);
-    let geode = game.tick(BuildOption::Geode, blueprint);
-
-    // Update the none option depending on what was built
-    let mut none = game.tick(BuildOption::None, blueprint);
-
-    if let Some(none) = none.as_mut() {
-        none.forbidden_builds.ore = none.forbidden_builds.ore || ore.is_some();
-        none.forbidden_builds.clay = none.forbidden_builds.clay || clay.is_some();
-        none.forbidden_builds.obsidian = none.forbidden_builds.obsidian || obsidian.is_some();
-        none.forbidden_builds.geode = none.forbidden_builds.geode || geode.is_some();
-    }
-
-    [ore, clay, obsidian, geode, none]
-        .into_iter()
-        .filter_map(|game| {
-            game.map(|game| simulate_game_rec(&game, current_time + 1, time, blueprint))
-        })
-        .max()
-        .unwrap()
-}
-
-pub fn simulate_game(time: usize, blueprint: &Blueprint) -> usize {
-    let game = Game::new();
-    simulate_game_rec(&game, 0, time, blueprint)
 }
 
 fn simulate_game_with_scout_rec(
@@ -410,67 +99,40 @@ fn simulate_game_with_scout_rec(
     blueprint: &Blueprint,
 ) -> usize {
     if current_time == time {
-        return game.resources.geode * blueprint.index;
+        return game.resources.geode;
     }
 
     // Get build option
     let build_options = game.build_options(current_time, max_time, blueprint);
 
     // Apply them recursively
-    build_options
-        .into_iter()
-        .flatten()
-        .map(|build_option| {
-            let mut game = game.clone();
-            game.tick_with_build_option(build_option, blueprint);
-            simulate_game_with_scout_rec(&mut game, current_time + 1, time, max_time, blueprint)
-        })
-        .max()
-        .unwrap()
+    if current_time <= 10 {
+        build_options
+            .into_par_iter()
+            .flatten()
+            .map(|build_option| {
+                let mut game = game.clone();
+                game.tick(build_option, blueprint);
+                simulate_game_with_scout_rec(&mut game, current_time + 1, time, max_time, blueprint)
+            })
+            .max()
+            .unwrap()
+    } else {
+        build_options
+            .into_iter()
+            .flatten()
+            .map(|build_option| {
+                let mut game = game.clone();
+                game.tick(build_option, blueprint);
+                simulate_game_with_scout_rec(&mut game, current_time + 1, time, max_time, blueprint)
+            })
+            .max()
+            .unwrap()
+    }
 }
 
 pub fn simulate_game_with_scout(time: usize, blueprint: &Blueprint) -> usize {
     let max_time = MaxTime::new(time, blueprint);
     let mut game = Game::new();
     simulate_game_with_scout_rec(&mut game, 0, time, &max_time, blueprint)
-}
-
-fn simulate_game_rec_heuristic(
-    game: &Game,
-    current_time: usize,
-    time: usize,
-    blueprint: &Blueprint,
-) -> usize {
-    if current_time == time {
-        return game.resources.geode;
-    }
-
-    // Get the game for every build option
-    let ore = game.tick_heuristic(BuildOption::Ore, blueprint);
-    let clay = game.tick_heuristic(BuildOption::Clay, blueprint);
-    let obsidian = game.tick_heuristic(BuildOption::Obsidian, blueprint);
-    let geode = game.tick_heuristic(BuildOption::Geode, blueprint);
-
-    // Update the none option depending on what was built
-    let mut none = game.tick_heuristic(BuildOption::None, blueprint);
-
-    if let Some(none) = none.as_mut() {
-        none.forbidden_builds.ore = none.forbidden_builds.ore || ore.is_some();
-        none.forbidden_builds.clay = none.forbidden_builds.clay || clay.is_some();
-        none.forbidden_builds.obsidian = none.forbidden_builds.obsidian || obsidian.is_some();
-        none.forbidden_builds.geode = none.forbidden_builds.geode || geode.is_some();
-    }
-
-    [ore, clay, obsidian, geode, none]
-        .into_iter()
-        .filter_map(|game| {
-            game.map(|game| simulate_game_rec_heuristic(&game, current_time + 1, time, blueprint))
-        })
-        .max()
-        .unwrap()
-}
-
-pub fn simulate_game_heuristic(time: usize, blueprint: &Blueprint) -> usize {
-    let game = Game::new();
-    simulate_game_rec_heuristic(&game, 0, time, blueprint)
 }
--- day19/src/main.rs
+++ day19/src/main.rs
@@ -1,11 +1,9 @@
-use rayon::prelude::*;
-
 use std::fs;
 use std::path::PathBuf;
 use std::time::Instant;
 
 use clap::Parser;
-use day19::logic::{simulate_game, simulate_game_heuristic, simulate_game_with_scout};
+use day19::logic::simulate_game_with_scout;
 use day19::models::Blueprint;
 use day19::parser::parse_data;
 
@@ -21,41 +19,25 @@ struct Cli {
 }
 
 fn solve_part_one(blueprints: &[Blueprint]) {
-    // let now = Instant::now();
-    //
-    // let res: usize = blueprints
-    //     .iter()
-    //     .map(|blueprint| simulate_game(24, blueprint))
-    //     .sum();
-    //
-    // println!(
-    //     "Part one solution (simulate_game): {} in {} ms",
-    //     res,
-    //     now.elapsed().as_millis()
-    // );
+    let res: usize = blueprints
+        .iter()
+        .map(|blueprint| simulate_game_with_scout(24, blueprint) * blueprint.index)
+        .sum();
 
+    println!("Part one solution {}", res,);
+}
+
+fn solve_part_two(blueprints: &[Blueprint]) {
     let now = Instant::now();
 
     let res: usize = blueprints
         .iter()
-        .map(|blueprint| simulate_game_with_scout(24, blueprint))
-        .sum();
-
-    println!(
-        "Part one solution (simulate_game_with_scout): {} in {} ms",
-        res,
-        now.elapsed().as_millis()
-    );
-}
-
-fn solve_part_two(blueprints: &[Blueprint]) {
-    let res: usize = blueprints
-        .par_iter()
         .take(3)
-        .map(|blueprint| simulate_game_heuristic(32, blueprint))
+        .map(|blueprint| simulate_game_with_scout(32, blueprint))
         .product();
 
     println!("Part two solution: {}", res);
+    println!("Elapsed time: {} ms", now.elapsed().as_millis());
 }
 
 fn main() {
@@ -65,5 +47,5 @@ fn main() {
     let blueprints = parse_data(&data);
 
     solve_part_one(&blueprints);
-    // solve_part_two(&blueprints);
+    solve_part_two(&blueprints);
 }
