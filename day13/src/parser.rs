97c1264d267330a42852f8794fb129425e7ebb31
arodrigue003 <adrien.rod@aliceadsl.fr>
2023-11-30 20:59:08 +0100

    Refactor: Improve day13 parser

--- day13/src/parser.rs
+++ day13/src/parser.rs
@@ -1,28 +1,28 @@
 use std::num::ParseIntError;
 use std::str::FromStr;
 
-use nom::{IResult, Parser};
 use nom::branch::alt;
 use nom::bytes::complete::tag;
 use nom::character::complete::{digit1, line_ending};
 use nom::combinator::{map, map_res, opt};
-use nom::multi::many0;
+use nom::multi::{many0, separated_list0};
 use nom::sequence::{delimited, terminated, tuple};
+use nom::{IResult, Parser};
 
 use crate::model::{PacketInner, PacketPair};
 
 pub fn parse_packet_inner(input: &str) -> IResult<&str, PacketInner> {
     alt((
-        map_res(terminated(digit1, opt(tag(","))), |data| {
+        map_res(digit1, |data| {
             Ok::<_, ParseIntError>(PacketInner::Value(i32::from_str(data)?))
         }),
         map(
             delimited(
                 tag("["),
-                many0(parse_packet_inner),
-                tuple((tag("]"), opt(tag(",")))),
+                separated_list0(tag(","), parse_packet_inner),
+                tag("]"),
             ),
-             PacketInner::List,
+            PacketInner::List,
         ),
     ))
     .parse(input)
